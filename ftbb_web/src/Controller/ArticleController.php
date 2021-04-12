900adab6d43a78ed0dcf84024d85c899c5805eb2
ahmed-debbech <debbech.ahmed@gmail.com>
2021-04-12 16:48:11 +0100

    fix forms

--- ftbb_web/src/Controller/ArticleController.php
+++ ftbb_web/src/Controller/ArticleController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Form\ArticleAddFormType;
+//use Symfony\Component\Validator\Constraints\DateTime;
 
 class ArticleController extends AbstractController
 {
@@ -16,6 +19,31 @@ class ArticleController extends AbstractController
     {
         $articles = $this ->getDoctrine()->getRepository(Article :: class)->findAll();
         return $this->render('article/articles.html.twig', ['articles' => $articles]);
+    }
+
+    /**
+     * @Route("/articles/add", name="add_article")
+     */
+    public function addArticle(Request $req): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleAddFormType::class, $article);
+
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this ->getDoctrine()->getManager();
+            $article->setAdminId("22221199");
+            $article->setArticleId("54409");
+            $article->setDate(date('Y-m-d'));
+            $em->persist($article);
+            $em->flush();
+            
+            //return $this->redirect('/articles');
+        }
+        return $this->render('article/admin/article-add-form.html.twig', [
+            'article_add_form' => $form->createView()
+        ]);
     }
 
     /**
@@ -34,4 +62,5 @@ class ArticleController extends AbstractController
         
         return 5;
     }
+
 }
