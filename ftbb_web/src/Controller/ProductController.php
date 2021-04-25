c99b160fb6c618ad8b9167c098b3ced87ffe4ea9
onskechrid <onskechrid1999@gmail.com>
2021-04-25 17:04:31 +0100

    fix total price in cart

--- ftbb_web/src/Controller/ProductController.php
+++ ftbb_web/src/Controller/ProductController.php
@@ -16,6 +16,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormTypeInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 
 class ProductController extends AbstractController
 {
@@ -244,6 +247,41 @@ class ProductController extends AbstractController
             'chart' => $ob
         ));
     }
+
+    /**
+     * @Route("/product/list_product_pdf", name="list_product_pdf")
+     */
+    public function pdf(ProductRepository $repository,Request $request): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        $data=$request->get('search');
+        $em=$repository->search($data);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('back/list_product_pdf.html.twig', [
+            'data' => $em,
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+
 
 
 
