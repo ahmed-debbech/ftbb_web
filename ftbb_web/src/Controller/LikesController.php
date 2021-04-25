200c10a187c47f170e2a4026af079b34ff32664e
ahmed-debbech <debbech.ahmed@gmail.com>
2021-04-25 02:31:11 +0100

    add metier option

--- ftbb_web/src/Controller/LikesController.php
+++ ftbb_web/src/Controller/LikesController.php
@@ -18,7 +18,7 @@ class LikesController extends AbstractController
     /**
      * @Route("/likes/click/comment/{com_id}/{art_id}", name="comment_like");
      */
-    public function onClickLike($com_id, $art_id){
+    public function onClickLike($com_id, $art_id, \Swift_Mailer $mailer){
         $em = $this ->getDoctrine()->getManager();
         $like = $this ->getDoctrine()->getRepository(Likes :: class)->findBy(array('idClient'=>122, 'idComment'=>$com_id));
         if(!empty($like)){
@@ -34,6 +34,16 @@ class LikesController extends AbstractController
             $like->setIdComment($comment);
             $em->persist($like);
             $em->flush();
+            if($comment->getClient()->getId() != $client->getId()){
+            $msg = (new \Swift_Message('[FTBB_SOCIAL] your comment got likes!'))
+                ->setFrom("ftbb.store@gmail.com")
+                ->setTo($comment->getClient()->getEmail())
+                ->setBody($this->render('email/body.html.twig', ['name' => $comment->getClient()->getName(), 'comment' => $comment->getContent()]))
+            ;
+            $mailer->send($msg);
+
+            $this->addFlash('message', 'Le message a bien été envoyé');
+            }
         }
         return $this->redirectToRoute("one_article", ['id' => $art_id]);
     }
