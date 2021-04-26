f57389e54fea977989af43761a45db2c9bb0e1e2
onskechrid <onskechrid1999@gmail.com>
2021-04-26 02:02:16 +0100

    fix bugs

--- ftbb_web/src/Controller/CartController.php
+++ ftbb_web/src/Controller/CartController.php
@@ -7,6 +7,7 @@ use App\Entity\Product;
 use App\Form\AjouterProductType;
 use App\Utils\Utilities;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -80,7 +81,7 @@ class CartController extends AbstractController
             $em->flush();
         }
 
-            return $this->redirectToRoute('list_product_client');
+            return new JsonResponse();
     }
 
     /**
