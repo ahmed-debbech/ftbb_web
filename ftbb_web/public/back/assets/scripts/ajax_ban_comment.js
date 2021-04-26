62c599073056ff4376d4c8a7197b79747b29997c
ahmed-debbech <debbech.ahmed@gmail.com>
2021-04-26 03:56:42 +0100

    format js file

--- ftbb_web/public/back/assets/scripts/ajax_ban_comment.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#ban').click(function() {
-        $.get('http://127.0.0.1/ftbb_web/ftbb_web/public/index.php/admin/article/{{comment.articleId}}/ban/{{comment.id}}', function(data, status) {
-            $('#passa').html("gogo");
-            alert(status)
-        });
-    });
-  });
-  alert('ffff');
